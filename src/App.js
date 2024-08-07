d51b738503f5e9237c8c2135e2ba19418989d1b6
Seonghyeon Kim <cc221051@fhstp.ac.at>
2024-08-07 22:02:44 +0200

    Added: Basic game logic, some changes in ui
    what is working:
    basically every basic feature is working: you can play, check who is winner, get scores
    
    TODO:
    make text color change on win condition,
    responsive button,
    responsive page,
    add sound

--- src/App.js
+++ src/App.js
@@ -3,12 +3,43 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  let [title, TitleChange] = useState(["성인 코트 추천", '강남우동', '파이썬독학', '홍대', '여의도']);
-  let[like, likeChange] = useState(0);
+  let [choices, choicesChange] = useState(["✊", "✌️", "✋"]);
+  let [choicesComputer, choicesComputerChange] = useState("✊");
+  let [checkWinText, checkWinTextChange] = useState("");
+  let[scoreComputer, scoreComputerChange] = useState(0);
+  let[scorePlayer, scorePlayerChange] = useState(0);
 
-  function likeButton(){
-    likeChange(like +1)
-    console.log("clicked")
+
+  // function likeButton(){
+  //   likeChange(like +1)
+  //   console.log("clicked")
+  // }
+  const randNum = Math.floor(Math.random() * 3);
+  let computerChoice = choices[randNum]
+  function computerTurn()
+  {
+    choicesComputerChange(computerChoice)
+  }
+
+  function checkWinCondition(playerChoice)
+  {
+    console.log( playerChoice, computerChoice)
+
+     if(playerChoice == computerChoice)
+     {
+      checkWinTextChange("DRAW");
+     } else if (
+      (playerChoice === "✊" && computerChoice === "✌️") ||
+      (playerChoice === "✌️" && computerChoice === "✋") ||
+      (playerChoice === "✋" && computerChoice === "✊")
+    ) {
+      checkWinTextChange("YOU WIN");
+      scorePlayerChange(scorePlayer + 1);
+    } else {
+      checkWinTextChange("YOU LOSE");
+      scoreComputerChange(scoreComputer + 1);
+    }
+
   }
 
 
@@ -24,31 +55,31 @@ function App() {
             <div className='card-container'>
             
               <div className='card-icon'>
-              <p>✊</p>
+              <p>{ choicesComputer }</p>
               </div>
             </div>
           </div>
         <div>
         <div className='score'>
             <div className='score-text'>
-              <h1>0</h1>
+              <h1>{ scoreComputer }</h1>
             </div>
             <div className='score-text'>
               <h1>:</h1>
             </div>
             <div className='score-text'>
-              <h1>0</h1>
+              <h1>{ scorePlayer }</h1>
             </div>
         </div>
         <div className='win-text'>
-          <h1>YOU WIN</h1></div>
+          <h1>{checkWinText}</h1></div>
         </div>
           <div className='player-container'>
           <h1>Player</h1>
             <div className='card-container'>
             
               <div className='card-icon'>
-              <p>✋</p>
+              <p>{ choices[0] }</p>
               </div>
 
             </div>
@@ -56,9 +87,27 @@ function App() {
         </div>
 
         <div className='buttons-group'>
-          <div className='button-card'>✊</div>
-          <div className='button-card'>✌️</div>
-          <div className='button-card'>✋</div>
+          <div className='button-card'><span onClick={ ()=> {
+            let copy = [...choices]
+            copy[0] = '✊';
+            choicesChange(copy);
+            computerTurn(); 
+            checkWinCondition('✊')
+            } }>✊</span></div>
+          <div className='button-card'><span onClick={ ()=> {
+             let copy = [...choices]
+             copy[0] = '✌️';
+             choicesChange(copy);
+             computerTurn();
+             checkWinCondition('✌️')
+            } }>✌️</span></div>
+          <div className='button-card'><span onClick={ ()=> {
+            let copy = [...choices]
+            copy[0] = '✋';
+            choicesChange(copy);
+            computerTurn();
+            checkWinCondition('✋')
+            } }>✋</span></div>
         </div>
 
         <div className='game-rule'>
@@ -86,7 +135,61 @@ function App() {
 
 
 
+{/* <div className="App">
+<div className="black-nav">
+  <h4>shifumi game </h4>
+</div>
+<button onClick={() => {
+  let copy = [...글제목];
+  copy.sort();
+  TitleChange(copy)
 
+}}>가나다순 정렬</button>
+
+
+
+
+
+<Modal/>
+
+
+</div>
+);
+}
+
+function List()
+{
+let post = '강남우동맛집';
+let [글제목, TitleChange] = useState(["성인 코트 추천", '강남우동', '파이썬독학']);
+let[like, likeChange] = useState(0);
+
+function likeButton(){
+likeChange(like +1)
+console.log("clicked")
+}
+<div className='list'>
+<h4>{ 글제목[0] } <span onClick={ likeButton }>👍</span> {like} </h4>
+<p>2월 15일 발행</p> 
+<button onClick={ ()=> {
+글제목.sort();
+
+let copy = [...글제목];
+copy[0] = '여자코트 추천';
+TitleChange(copy) 
+}}>버튼</button>
+</div> 
+}
+
+function Modal()
+{
+return (
+<div className='modal'>
+<h4>제목</h4>
+<p>날짜</p>
+<p>상세내용</p>
+</div>
+)
+} */}
 
 function Modal()
 {
